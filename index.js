e9703725f43a9c52a5c5c6acbb6a222a04478fcf
Jean Carlos  Ramos <jeancarlosramos@Jeans-MacBook-Pro.local>
2020-08-25 08:52:10 -0400

    task 1 completed

--- index.js
+++ index.js
@@ -40,11 +40,13 @@ To save you from having to count the items above, you can assume that length of 
 
 i.e. is31Flavors(originalFlavors) will return TRUE.*/
 
-function is31Flavors(/*code here*/){
-
-    /*code here*/
-
+function is31Flavors(arr){
+        if (arr.length === 31){
+            console.log(`TRUE! originalFlavors is ${arr.length}!!!`);
+        }
 }
+
+// is31Flavors(originalFlavors);
 
 /* Task 2: Corporate has come to you with an idea for a new flavor: Rainbow Sherbert! They think this will be a game changer. You need to modify the array to include this flavor. 
 
@@ -57,7 +59,7 @@ Your function should add the flavor to the front of the array and console.log th
 
 For example addFlavor("Rainbow Sherbert", originalFlavors) should return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla Burnt Almond"] */ 
 
-function addFlavor(/*code here*/){
+function addFlavor(fla){
 
     /*code here*/
 
